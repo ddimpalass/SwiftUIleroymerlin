2e80e796e648cdbac44a4988f55a73debe7eb5b6
Дмитрий Щелочков <ddimpalass@icloud.com>
2021-04-26 09:22:43 +0300

    Initial Commit

---
+++ SwiftUIleroymerlin/SwiftUIleroymerlinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIleroymerlinApp.swift
+//  SwiftUIleroymerlin
+//
+//  Created by Apple on 26.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIleroymerlinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
